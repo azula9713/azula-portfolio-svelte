fe04008b49e1ee8092145b98fb2ea251c5c09c19
Nilesh Viduranga <nnviduranga@gmail.com>
2023-11-02 22:28:12 +0530

    Updated timeline icon

--- src/lib/components/WorkHistory.svelte
+++ src/lib/components/WorkHistory.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { Timeline, TimelineItem, Button } from 'flowbite-svelte';
+	import { Timeline, TimelineItem } from 'flowbite-svelte';
+	import { CalendarWeekSolid } from 'flowbite-svelte-icons';
 
 	import Title from './common/Title.svelte';
 
@@ -12,9 +13,16 @@
 <section class="flex flex-col items-start justify-center w-full pt-4 lg:pt-12">
 	<Title title="Work History" />
 	<div class="flex flex-col items-center justify-center w-full pt-4">
-		<Timeline>
+		<Timeline order="vertical">
 			{#each workHistory as item}
 				<TimelineItem title={item.title} date={item.date}>
+					<svelte:fragment slot="icon">
+						<span
+							class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-4 ring-secondary bg-primary"
+						>
+							<CalendarWeekSolid class="w-3 h-3 text-secondary" />
+						</span>
+					</svelte:fragment>
 					<ul class={ulStyle}>
 						{#each item.work as work}
 							<li class={listItemStyle}>{work.task}</li>
