d374ba685034db5d5fd3712629a1860f7a65673e
Nilesh Viduranga <nnviduranga@gmail.com>
2023-11-10 22:32:52 +0530

    Updated projects with real data

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDAuGsMQN5q_Vq-Shz88QJQwQYr8pQpN4s',
+	authDomain: 'portfolio-6f0dd.firebaseapp.com',
+	projectId: 'portfolio-6f0dd',
+	storageBucket: 'portfolio-6f0dd.appspot.com',
+	messagingSenderId: '304503340536',
+	appId: '1:304503340536:web:1fa6a0c0222bd6cea6aa0d'
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
