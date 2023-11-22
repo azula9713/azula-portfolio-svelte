2886fc87ccd7e4a326cbef9db8cd06522f65cfb5
Azula9713 <nnviduranga@gmail.com>
2023-11-22 09:09:31 +0530

    Add axios dependency and import RecentArticles component

---
+++ src/lib/components/RecentArticles.svelte
@@ -0,0 +1,116 @@
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	import { quintOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+
+	import Title from './common/Title.svelte';
+
+	/**
+	 * @type {{ title: string; link: string; pubDate: string; description: string; image: string }[]}
+	 */
+	let articles = [];
+
+	const rssURL =
+		'https://cors-anywhere.herokuapp.com/https://blog.yaepublishinghouse.online/rss.xml';
+
+	// function parseElement(element) {
+	//     const reg
+
+	onMount(() => {
+		axios.get(rssURL).then((res) => {
+			const parser = new DOMParser();
+			const xml = parser.parseFromString(res.data, 'text/xml');
+			const items = xml.querySelectorAll('item');
+
+			const maxArticles = 3;
+			/**
+			 * @type {{ title: string; link: string; pubDate: string; description: string; image: string; }[]}
+			 */
+			let temp = [];
+
+			items.forEach((item) => {
+				/**
+				 * @typedef {Object} Article
+				 * @property {string} title - The article's title.
+				 * @property {string} link - The article's link.
+				 * @property {string} pubDate - The article's publication date.
+				 * @property {string} description - The article's description.
+				 * @property {string} image - The article's image.
+				 *
+				 */
+
+				const titleElement = item.querySelector('title');
+				const linkElement = item.querySelector('link');
+				const pubDateElement = item.querySelector('pubDate');
+				const descriptionElement = item.querySelector('description');
+				const imageElement = item.querySelector('cover_image');
+
+				if (
+					titleElement &&
+					linkElement &&
+					pubDateElement &&
+					descriptionElement &&
+					imageElement &&
+					temp.length < maxArticles
+				) {
+					temp.push({
+						title: titleElement.innerHTML.replace('<![CDATA[', '').replace(']]>', ''),
+						link: linkElement.innerHTML,
+						pubDate: pubDateElement.innerHTML,
+						description: descriptionElement.innerHTML
+							.replace('<![CDATA[', '')
+							.replace(']]>', '')
+							// limit to 67 words
+							.split(' ')
+							.slice(0, 67)
+							.join(' ')
+							// append ellipsis
+							.concat('...'),
+
+						image: imageElement.innerHTML
+					});
+					console.log('articles', articles);
+				}
+			});
+
+			articles = temp;
+		});
+	});
+</script>
+
+<section
+	transition:fade={{ duration: 1000, delay: 2500, easing: quintOut }}
+	class="flex flex-col items-start justify-center w-full pt-4 lg:pt-12"
+>
+	<Title title="Recent Articles" />
+
+	<div class="flex flex-col items-center justify-center w-full pt-4">
+		<div class="flex flex-col items-start justify-center space-x-0 space-y-8 overflow-hidden">
+			{#each articles as article}
+				<div class="flex flex-col items-center justify-center w-full px-4 py-8 space-y-4">
+					<h1 class="text-2xl font-bold text-gray-700 dark:text-gray-200 text-left w-full">
+						{article.title}
+					</h1>
+					<!-- display image and description side by side -->
+					<div class="flex flex-col lg:flex-row items-start justify-center w-full space-x-4">
+						<img src={article.image} alt={article.title} class="w-1/2" />
+						<div class="h-full flex flex-col items-stretch">
+							<div class="text-gray-600 dark:text-gray-300 mt-2 text-left">
+								{@html article.description}
+							</div>
+							<a
+								href={article.link}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="text-primary hover:underline font-bold pt-2"
+							>
+								Read more
+							</a>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
 
-	import Button from '$lib/components/common/Button.svelte';
 	import ConnectWithMe from '$lib/components/ConnectWithMe.svelte';
 	import Intro from '$lib/components/Intro.svelte';
 	import Projects from '$lib/components/Projects.svelte';
+	import RecentArticles from '$lib/components/RecentArticles.svelte';
 	import TechStack from '$lib/components/TechStack.svelte';
 	import WorkHistory from '$lib/components/WorkHistory.svelte';
+	import Button from '$lib/components/common/Button.svelte';
 	import { sideBarState } from '$lib/stores';
 
 	let ready = false;
@@ -34,6 +35,7 @@
 		<TechStack />
 		<Projects />
 		<WorkHistory />
+		<RecentArticles />
 		<ConnectWithMe />
 	{/if}
 </div>
