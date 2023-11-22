f79b3b68b9b097f7e291a3216c98ff508381ea51
Nilesh Viduranga <nnviduranga@gmail.com>
2023-11-22 19:15:54 +0530

    Dev (#9)
    * Mapped Socials and added functionality to let's talk button
    
    * Moved work history to js object
    
    * Updated timeline icon
    
    * Added meta tags and minor css fixes
    
    * Updated projects with real data
    
    * Update favicon.ico
    
    * Update ConnectWithMe component and work history
    
    * Added logo image
    
    * Add axios dependency and import RecentArticles
    component
    
    * Update dependencies in package.json
    
    * Refactor RecentArticles component to separate
    development and production RSS URLs
    
    * Refactor RecentArticles component to use fetch
    instead of axios
    
    * Remove axios dependency from package.json
    
    * Add console log for current node environment
    
    * Add fast-xml-parser dependency and update
    RecentArticles component
    
    * Add Firebase configuration files and Cloud
    Function for fetching RSS feed
    
    * Remove fast-xml-parser dependency and update RSS
    URL
    
    * Remove console.log statement in
    RecentArticles.svelte
    
    * Refactor Drawer component and add form submission
    functionality

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -1,8 +1,9 @@
 <script>
-	import { onDestroy } from 'svelte';
-	import { Drawer, Button, CloseButton, Label, Input, Textarea } from 'flowbite-svelte';
-	import { sineIn } from 'svelte/easing';
+	import { Button, CloseButton, Drawer, Input, Label, Textarea } from 'flowbite-svelte';
 	import { DownloadSolid } from 'flowbite-svelte-icons';
+	import { sineIn } from 'svelte/easing';
+
+	import qrCode from '$lib/images/download.png';
 
 	import { sideBarState } from '$lib/stores';
 
@@ -11,6 +12,8 @@
 		duration: 200,
 		easing: sineIn
 	};
+
+	const STATIC_FORM_ACCESS_KEY = import.meta.env.VITE_STATIC_FORM_ACCESS_KEY;
 </script>
 
 <Drawer transitionType="fly" {transitionParams} bind:hidden={$sideBarState} placement="right" out>
@@ -27,15 +30,68 @@
 			class="mb-4 dark:text-white"
 		/>
 	</div>
-	<Button type="button" class=" bg-primary w-full mb-6">
+	<Button
+		type="button"
+		class=" bg-primary w-full mb-3"
+		href="https://file.io/SAl6tqpTsUlW"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
 		<DownloadSolid class="w-4 h-4 mr-2" />
 		Download Resume
 	</Button>
 
-	<form action="#" class="mb-2">
+	<div
+		class="text-xs font-semibold text-gray-500 dark:text-gray-400 mb-6 w-full flex flex-col items-center justify-center"
+	>
+		<p>Doesn't work? Scan this QR code with your phone</p>
+		<img src={qrCode} alt="QR code" class="w-1/2 mt-2" />
+	</div>
+
+	<form
+		action="https://api.staticforms.xyz/submit"
+		method="post"
+		class="mb-2"
+		id="talkform"
+		on:submit={(e) => {
+			e.preventDefault();
+
+			/**
+			 * @type {any}
+			 *
+			 */
+			const form = document.getElementById('talkform');
+
+			if (!form && form instanceof HTMLFormElement === false)
+				return console.error('Form not found');
+
+			const formData = new FormData(form);
+			const bodyObj = Object.fromEntries(formData);
+
+			fetch('https://api.staticforms.xyz/submit', {
+				method: 'POST',
+				body: JSON.stringify(bodyObj),
+				headers: { 'Content-Type': 'application/json' }
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					if (data.success) {
+						alert('Your message has been sent!');
+						form.reset();
+					} else {
+						alert('Oops! Something went wrong');
+					}
+				});
+		}}
+	>
+		<input type="hidden" name="accessKey" value={STATIC_FORM_ACCESS_KEY} />
 		<div class="mb-6">
 			<Label for="email" class="block mb-2">Your email</Label>
 			<Input id="email" name="email" required placeholder="name@company.com" />
+		</div>
+		<div class="mb-6">
+			<Label for="name" class="block mb-2">Your name</Label>
+			<Input id="name" name="name" required placeholder="John Doe" />
 		</div>
 		<div class="mb-6">
 			<Label for="subject" class="block mb-2">Subject</Label>
